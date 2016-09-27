a7275f3e28dfbc872936e1075f8a288a618a059b
Zeitlin1 <Zeitlin1@gmail.com>
2016-09-27 15:10:16 -0400

    Add files via upload
    add solutions and pull

---
+++ ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  SimonSaysLab
+//
+//  Created by James Campagno on 5/31/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayColorView: UIView!
+    @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var winLabel: UILabel!
+    var simonSaysGame = SimonSays()
+    var buttonsClicked = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func redButton(_ sender: AnyObject) {
+        simonSaysGame.guessRed()
+        
+    }
+    
+    @IBAction func greenButton(_ sender: AnyObject) {
+        simonSaysGame.guessGreen()
+    }
+    
+    @IBAction func yellowButton(_ sender: AnyObject) {
+        simonSaysGame.guessYellow()
+    }
+    
+    @IBAction func blueButton(_ sender: AnyObject) {
+        simonSaysGame.guessBlue()
+    }
+    
+}
+
+// MARK: - SimonSays Game Methods
+extension ViewController {
+    
+    @IBAction func startGameTapped(_ sender: UIButton) {
+        UIView.transition(with: startGameButton, duration: 0.9, options: .transitionFlipFromBottom , animations: {
+            self.startGameButton.isHidden = true
+            }, completion: nil)
+        
+        displayTheColors()
+    }
+    
+    fileprivate func displayTheColors() {
+        self.view.isUserInteractionEnabled = false
+        UIView.transition(with: displayColorView, duration: 1.5, options: .transitionCurlUp, animations: {
+            self.displayColorView.backgroundColor = self.simonSaysGame.nextColor()?.colorToDisplay
+            self.displayColorView.alpha = 0.0
+            self.displayColorView.alpha = 1.0
+            }, completion: { _ in
+                if !self.simonSaysGame.sequenceFinished() {
+                    self.displayTheColors()
+                } else {
+                    self.view.isUserInteractionEnabled = true
+                    print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
+                }
+        })
+    }
+    
+    // code the rest of the button functions here
+}
